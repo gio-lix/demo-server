022163403e943e1d2243305fd7e99706b0b358e0
Giorgi Mosemgvdlishvili <gmosemgvdlishvili28@gmail.com>
2022-11-21 11:41:54 +0400

    add mongo

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,20 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import express, {Express, Request, Response} from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+import cookieParser from 'cookie-parser'
 
+import './config/dataBase'
 
 const app: Express = express();
 const port = process.env.PORT || 5005;
 
-
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(morgan('dev'))
+app.use(cookieParser())
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
