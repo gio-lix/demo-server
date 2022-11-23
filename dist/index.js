178410943fb01319175ca80bbed5dcafc3325a84
Giorgi Mosemgvdlishvili <gmosemgvdlishvili28@gmail.com>
2022-11-23 12:58:27 +0400

    update dis

--- dist/index.js
+++ dist/index.js
@@ -15,17 +15,13 @@ const morgan_1 = __importDefault(require("morgan"));
 const index_1 = __importDefault(require("./routes/index"));
 require("./config/dataBase");
 const app = (0, express_1.default)();
-const endpoints = [
-    'http://localhost:3000',
-    'https://aaaapppp12-app.onrender.com',
-];
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use((0, morgan_1.default)('dev'));
 app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)({
     credentials: true,
-    origin: endpoints
+    origin: "*"
 }));
 const http = (0, http_1.createServer)(app);
 exports.io = new socket_io_1.Server(http);
